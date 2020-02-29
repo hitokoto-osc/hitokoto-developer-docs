9e631935c054ed8e4da3617343e0e3f3c1be7d9f
a632079 <a632079@qq.com>
2020-03-01 03:17:19 +0800

    feat: pwa support

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,15 @@ module.exports = {
   description: "一言，用代码表达言语的魅力，用代码书写山河的壮丽。本文档提供一言开发接口的使用说明，SDK，常见问题。",
   base: "/",
   head: [
-    ["link", { rel: "icon", href: "/favicon.ico" }]
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/logo.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/logo.svg', color: '#8921ff' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/logo.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ],
   markdown: {
     lineNumbers: true
@@ -73,6 +81,13 @@ module.exports = {
     ],
     ['vuepress-plugin-smooth-scroll'],
     ['@vuepress/back-to-top'],
-    ['@vuepress/active-header-links']
+    ['@vuepress/active-header-links'],
+    ['@vuepress/pwa', {
+      erviceWorker: true,
+      updatePopup: {
+        message: "文章内容已更新。",
+        buttonText: "刷新页面"
+      }
+    }]
   ]
 };
