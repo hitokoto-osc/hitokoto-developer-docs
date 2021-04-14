7ccb69404c7b4fa0cb25b2293e71421316efa695
greenhat616 <i@a632079.me>
2021-04-14 21:22:09 +0800

    fix: base path to adapt deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: "一言开发者中心",
     description: "一言，用代码表达言语的魅力，用代码书写山河的壮丽。本文档提供一言开发接口的使用说明，SDK，常见问题。",
-    base: "hitokoto-developer-docs/",
+    base: "/",
     head: [
         ["link", { rel: "icon", href: "/favicon.ico" }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
